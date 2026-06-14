c52f5ca07e1767c9fca19151757d4eddf8056882
Cerilas <deniz@cerilas.com>
2026-06-14 23:47:45 +0300

    Initial Commit

---
+++ navikont/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  navikont
+//
+//  Created by Deniz on 14.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ navikont/navikontApp.swift
@@ -0,0 +1,17 @@
+//
+//  navikontApp.swift
+//  navikont
+//
+//  Created by Deniz on 14.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct navikontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
